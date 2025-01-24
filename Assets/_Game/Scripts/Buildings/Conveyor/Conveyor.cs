69400cc58dc858d7e9281ff9096bf696d479ea6b
Tomasz Jaworski <71073337+TomaszJaworski777@users.noreply.github.com>
2025-01-24 23:12:13 +0100

    Conveyor art

--- Assets/_Game/Scripts/Buildings/Conveyor/Conveyor.cs
+++ Assets/_Game/Scripts/Buildings/Conveyor/Conveyor.cs
@@ -1,13 +1,17 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class Conveyor : Building
 {
     [Header("Conveyor Settings")]
     [SerializeField]
     private float _conveyorSpeed;
-    
+
+    public List<Transform> Content { get; private set; }
+
     protected override void OnInit()
     {
+        Content = new List<Transform>();
         ConveyorScheduler.Instance.RegisterConveyor(this);
     }
 
