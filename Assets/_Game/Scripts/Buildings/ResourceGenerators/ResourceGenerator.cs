1ae76088b4bf8c6fa3182ffec9bce49cbabb0d75
Tomasz Jaworski <71073337+TomaszJaworski777@users.noreply.github.com>
2025-01-25 12:15:17 +0100

    Pole na prefab

--- Assets/_Game/Scripts/Buildings/ResourceGenerators/ResourceGenerator.cs
+++ Assets/_Game/Scripts/Buildings/ResourceGenerators/ResourceGenerator.cs
@@ -4,15 +4,27 @@ public class ResourceGenerator : Building
 {
     [Header("Generation Settings")] 
     [SerializeField]
-    private GameObject _resourcePrefab;
+    private Resource _resourcePrefab;
     [SerializeField]
     private float _frequency;
+
+    private float _timer;
     
     public override bool CanTakeItem(Resource item) => false;
     public override void TakeItem(Resource item) { }
     
     public override void ExecuteStep(float deltaTime)
     {
-        throw new System.NotImplementedException();
+        var resourceCount = 1;
+        
+        //detect resources under the building
+        
+        _timer += Time.deltaTime;
+        if ( _timer < 1f / (_frequency * resourceCount) )
+            return;
+        
+        _timer -= 1f / (_frequency * resourceCount);
+        
+        //spawn resrouce
     }
 }
--- Assets/_Game/Scripts/Resources/ResourceOre.cs
+++ Assets/_Game/Scripts/Resources/ResourceOre.cs
@@ -2,5 +2,5 @@
 
 public abstract class ResourceOre : MonoBehaviour
 {
-    
+    public Resource ResourcePrefab;
 }
