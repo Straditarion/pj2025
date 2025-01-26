07563e8883690d42c8f3429b6cebc77278678532
Tomasz Jaworski <71073337+TomaszJaworski777@users.noreply.github.com>
2025-01-26 10:50:36 +0100

    WIP Global Resources

--- Assets/_Game/Scripts/Resources/Resource.cs
+++ Assets/_Game/Scripts/Resources/Resource.cs
@@ -2,5 +2,8 @@
 
 public class Resource : MonoBehaviour
 {
-    
+    public int OrderIndex;
+    public SpriteRenderer SpriteRenderer => transform.GetChild(0).GetComponent<SpriteRenderer>();
+    public Sprite Sprite => SpriteRenderer.sprite;
+    public string Name => gameObject.name.Replace("(Clone)", "").Trim();
 }
