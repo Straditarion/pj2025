7b9e68bd983c386f2dbd8f7c0f73b5aa16157aa6
Tomasz Jaworski <71073337+TomaszJaworski777@users.noreply.github.com>
2025-01-26 15:35:39 +0100

    Fixed UI

--- Assets/_Game/Scripts/UI/ShopController.cs
+++ Assets/_Game/Scripts/UI/ShopController.cs
@@ -1,13 +1,9 @@
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public class ShopController : MonoBehaviour
 {
-    public GameObject CancelButton;
-    
     public void ChangePrefab(Building prefab)
     {
         Player.Instance.GetSystem<BuildingSystem>().ChangePrefab(prefab);
-        CancelButton.SetActive(prefab != null);
     }
 }
