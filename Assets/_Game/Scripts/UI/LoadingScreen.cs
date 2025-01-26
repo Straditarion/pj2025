ef79395797e02b89b50044872ce63ca345c375d6
Tomasz Jaworski <71073337+TomaszJaworski777@users.noreply.github.com>
2025-01-26 13:57:10 +0100

    Basic main menu

---
+++ Assets/_Game/Scripts/UI/LoadingScreen.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadingScreen : MonoBehaviour
+{
+    public static LoadingScreen Instance;
+    
+    [SerializeField] private float _fadeSpeed;
+    [SerializeField] private Image _image;
+
+    private void Awake()
+    {
+        Instance = this;
+
+        _image.raycastTarget = false;
+        
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void StartLoading()
+    {
+        _image.color = new Color(0, 0, 0, 1);
+        _image.raycastTarget = true;
+    }
+
+    public void Fade()
+    {
+        _image.raycastTarget = false;
+        StartCoroutine(FadeCoroutine());
+    }
+
+    private IEnumerator FadeCoroutine()
+    {
+        _image.color = new Color(0, 0, 0, _image.color.a - (_fadeSpeed * Time.deltaTime));
+        
+        yield return new WaitForNextFrameUnit();
+        
+        if(_image.color.a > 0)
+            StartCoroutine(FadeCoroutine());
+    }
+}
--- Assets/_Game/Scripts/UI/MainMenuController.cs
+++ Assets/_Game/Scripts/UI/MainMenuController.cs
@@ -5,14 +5,17 @@ public class MainMenuController : MonoBehaviour
 {
     public void Play()
     {
+        LoadingScreen.Instance.StartLoading();
+        
         SceneManager.LoadSceneAsync(1, LoadSceneMode.Additive);
         var gameplay = SceneManager.LoadSceneAsync(2, LoadSceneMode.Additive);
-
+        
         if (gameplay != null)
             gameplay.completed += operation =>
             {
                 SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene());
                 SceneManager.SetActiveScene(SceneManager.GetSceneByBuildIndex(2));
+                LoadingScreen.Instance.Fade();
             };
     }
 
